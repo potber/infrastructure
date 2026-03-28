703b13fe448f5d1fdf533308b7d22e4411b573ef
Kristof Dreier <mail@kristofdreier.de>
2026-03-28 15:32:45 +0100

    Add test potber-client deployment

---
+++ kubernetes/potber/overlays/prod/potber-client-test-config.js
@@ -0,0 +1,7 @@
+window.APP = window.APP || {};
+window.APP.HOSTNAME = window.location.host;
+window.APP.API_URL = 'https://api.potber.de';
+window.APP.IMGPOT_URL = 'https://imgpot.de';
+window.APP.AUTH_ISSUER_URL = 'https://auth.potber.de';
+window.APP.AUTH_CLIENT_ID = '45a14ddc-e3d3-4b5b-a45a-a04946974adc';
+window.APP.MEME_HOST_URL = window.location.origin;
