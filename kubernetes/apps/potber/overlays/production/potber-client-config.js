d89cb521dd22e38c6bd8fcd6eca0691853179020
Kristof Dreier <mail@kristofdreier.de>
2026-04-05 12:00:49 +0200

    Configure AppSignal for potber-client

--- kubernetes/apps/potber/overlays/production/potber-client-config.js
+++ kubernetes/apps/potber/overlays/production/potber-client-config.js
@@ -4,4 +4,5 @@ window.APP.API_URL = 'https://api.potber.de';
 window.APP.IMGPOT_URL = 'https://imgpot.de';
 window.APP.AUTH_ISSUER_URL = 'https://auth.potber.de';
 window.APP.AUTH_CLIENT_ID = '45a14ddc-e3d3-4b5b-a45a-a04946974adc';
+window.APP.APPSIGNAL_FRONTEND_KEY = 'e81d4b85-0d69-4ba0-ae4c-bba4b815659c';
 window.APP.MEME_HOST_URL = window.location.origin;
--- kubernetes/apps/potber/overlays/test/potber-client-config.js
+++ kubernetes/apps/potber/overlays/test/potber-client-config.js
@@ -4,4 +4,5 @@ window.APP.API_URL = 'https://test-api.potber.de';
 window.APP.IMGPOT_URL = 'https://imgpot.de';
 window.APP.AUTH_ISSUER_URL = 'https://test-auth.potber.de';
 window.APP.AUTH_CLIENT_ID = '45a14ddc-e3d3-4b5b-a45a-a04946974adc';
+window.APP.APPSIGNAL_FRONTEND_KEY = '58063698-a53e-4e0c-8a99-7243fed84b93';
 window.APP.MEME_HOST_URL = window.location.origin;
